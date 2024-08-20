0fad3cb4e473883f51de8d8789af116161d96a07
Maximilian Huber <maximilian.hub@proton.me>
2024-08-20 15:45:00 +0200

    created basic lexer

---
+++ cmd/gint/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"gint/internal/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type any commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module gint
+
+go 1.21.5
